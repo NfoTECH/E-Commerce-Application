20beef0db1ed7cc4e8064b0dc4c34fa3aa85d089
NfoTECH <fortunenwachukwu@gmail.com>
2022-10-26 21:14:32 +0100

    Product model modified

--- src/main/java/com/fortunate/nwachukwu/week6tasknfotech/model/Product.java
+++ src/main/java/com/fortunate/nwachukwu/week6tasknfotech/model/Product.java
@@ -14,7 +14,6 @@ public class Product {
 
 
     public Product(int id, String name, String category, Double price, int quantity, String image) {
-        super();
         this.id = id;
         this.name = name;
         this.category = category;
